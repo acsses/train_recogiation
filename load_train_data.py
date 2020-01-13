c72c4fea15c31bd9f831e841f5426d8954fea32a
Eagle Code <tomoki.takaba@outlook.jp>
2020-01-13 14:26:42 +0900

    load_train_data.pyの中身を関数化

--- load_train_data.py
+++ load_train_data.py
@@ -2,15 +2,16 @@ from PIL import Image
 import numpy as np
 import os
 
-data_array = np.asarray([])
-target_arrry = np.array([])
 
-main_folder = os.listdir("./")
-for sub_folder in main_folder:
-    data_ist = os.listdir(sub_folder)
-    for data in sub_folder:
-        img = Image.open(data)
-        fix_img = img.convert('L')
-        data_grey = np.asarray(fix_img)
-        target_arrry = np.append(target_arrry,sub_folder)
-        data_array = np.append(data_array,data_grey,axis=0)
+def load_func():
+    data_array = np.asarray([])
+    target_arrry = np.array([])
+    main_folder = os.listdir("./")
+    for sub_folder in main_folder:
+        data_ist = os.listdir(sub_folder)
+        for data in sub_folder:
+            img = Image.open(data)
+            fix_img = img.convert('L')
+            data_grey = np.asarray(fix_img)
+            target_arrry = np.append(target_arrry,sub_folder)
+            data_array = np.append(data_array,data_grey,axis=0)
