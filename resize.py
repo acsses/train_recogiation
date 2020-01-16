bed873b6b8ee30314ac7925c20f8664496f92bc8
Eagle Code <tomoki.takaba@outlook.jp>
2020-01-16 21:28:45 +0900

    resize.pyの修正完了

--- resize.py
+++ resize.py
@@ -6,5 +6,5 @@ for filename in folder:
     full_name = "./Type223Seris1000/" + filename
     im = Image.open(full_name)
     width,height=8,8
-    img = im.resize((width,height))
+    img = im.resize((width,height),Image.ANTIALIAS)
     img.save(full_name)
