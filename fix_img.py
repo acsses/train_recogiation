3b9a7875484fdec187423f5ca9edb3e103d5879f
Eagle Code <tomoki.takaba@outlook.jp>
2020-01-20 20:23:49 +0900

    コードを一部修正

--- fix_img.py
+++ fix_img.py
@@ -10,15 +10,8 @@ print(folder)
 for filename in folder:
     full_name = path[1] + "/" + filename
     im = Image.open(full_name)
-<<<<<<< HEAD
-    fix_img = im.convert('L')
-    cropped = fix_img.crop(fix_img.getbbox())
-    width,height=64,64
-    img = cropped.resize((width,height),Image.ANTIALIAS)
-=======
     cropped = im.crop(im.getbbox())
     fix_img = cropped.convert('L')
-    width,height=32,32
-    img = fix_img.resize((width,height).Image.ANTIALIAS)
->>>>>>> master
+    width,height=64,64
+    img = fix_img.resize((width,height),Image.ANTIALIAS)
     img.save(full_name)
