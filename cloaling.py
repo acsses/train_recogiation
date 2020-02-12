fa2132121652d22d393dc11ed29b931b45e42811
Eagle Code <tomoki.takaba@outlook.jp>
2020-02-12 16:15:13 +0900

    2クラス追加

--- cloaling.py
+++ cloaling.py
@@ -2,4 +2,4 @@
 from icrawler.builtin import GoogleImageCrawler
 
 crawler = GoogleImageCrawler(storage={"root_dir": "images"})
-crawler.crawl(keyword="321系", max_num=1500)
+crawler.crawl(keyword="323系", max_num=1500)
